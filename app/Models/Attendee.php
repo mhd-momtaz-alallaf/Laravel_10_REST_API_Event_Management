b1ef2fd88a06fd09c84ccdafcc22d04205a6febe
Alallaf1 <parental.co122@gmail.com>
2024-04-14 19:38:05 +0300

    edit the migrations and the models relations for Event and Attendee models

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendee extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class); // one user can attend many events.
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class); // the other side of the relation.
+    }
 }
--- database/migrations/2024_04_14_155801_create_attendees_table.php
+++ database/migrations/2024_04_14_155801_create_attendees_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
+
+            $table->forignIdFor(User::class); // adds the column and the forignKey for it.
+            // 1 to many relation. user can attend many events, the event can atended by one user.
+            $table->forignIdFor(Event::class);
+            
             $table->timestamps();
         });
     }
